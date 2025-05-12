6eee81a5f4cfc3602980b46a379417a30fdd84db
ArtemFurhas <af0990242922@gmail.com>
2025-05-12 18:54:50 +0300

    change server

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,8 +1,8 @@
 import { VibeFilters } from "../types/vibe"
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// const BASE_URL = "https://backend-muvio.onrender.com/api"
-const BASE_URL = "https://muvio.fly.dev/api"
+const BASE_URL = "https://backend-muvio.onrender.com/api"
+// const BASE_URL = "https://muvio.fly.dev/api"
 
 function wait(delay: number) {
   return new Promise(resolve => setTimeout(resolve, delay))
